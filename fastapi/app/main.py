e87c9bec2f1bc5ad124582b0e1110bcbd941e1cb
matteo.biglioli <bigliolimatteo@gmail.com>
2021-05-09 09:51:11 +0200

    fix cvs bugs

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,22 +1,24 @@
 from typing import Optional
-
+import io
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
 from app.postgresql_utils import PostgreSQLManager, Query
 
 app = FastAPI()
 db = PostgreSQLManager()
 
 @app.get("/test")
-    async def get_csv(name: str):
-        
-        query='''
-            SELECT id, name, geometry FROM tb_nations WHERE name = %s
-            '''
+async def get_csv(name: str):
+    query='''
+        SELECT id, name, geometry FROM tb_nations WHERE name = %s
+        '''
 
-        df = db.query_execute(Query(query, (name,)), fetch=True, asdataframe=True)
-        stream = io.StringIO()
-        df.to_csv(stream, index = False)
+    df = db.query_execute(Query(query, (name,)), fetch=True, asdataframe=True)
+    stream = io.StringIO()
+    df.to_csv(stream, index = False)
 
-        response = StreamingResponse(iter([stream.getvalue()]),media_type="text/csv")
-        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
-        return response
+    response = StreamingResponse(iter([stream.getvalue()]),media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    return response
+
+#35.198.107.181/test?name=test
