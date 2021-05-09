e16845c3e17bea2bbde41506acf9b9e1c193e9ff
matteo.biglioli <bigliolimatteo@gmail.com>
2021-05-09 13:09:52 +0200

    small bugs

--- download_all_data_to_db.sh
+++ download_all_data_to_db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo apt install python3-pip
+
 pip install -r scripts/requirements.txt
 
 python scripts/downloader.py --region lombardia
 
 cp -r scripts/raw_data postgresql/files/
 
-sh postgresql/utils/load_raw_data.sh
+sudo -E docker exec -it skynet_db sh /files/load_raw_data.sh
--- postgresql/utils/exec.sh
+++ postgresql/utils/exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # sudo -E docker exec -it postgresql_psql_postgis_db_1 /bin/bash
-container=${1:-skynet_sb}
+container=${1:-skynet_db}
 sudo -E docker exec -it $container /bin/bash
